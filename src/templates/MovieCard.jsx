eadac76fd7cd12f3340d4eda1e825f54ce691c4d
sofiehilge <sofie.hilge@gmail.com>
2023-04-21 14:18:08 +0200

    add styling to headings

--- src/templates/MovieCard.jsx
+++ src/templates/MovieCard.jsx
@@ -2,14 +2,18 @@ import Heading3 from "../components/Heading3";
 import Image from "../components/Image"; // Image navnet er ligemeget, men det skal hedde det samme i import og i component tagget.
 import MovieRating from "../components/MovieRating";
 
+
+
 const MovieCard = () => {
-    return ( <article>
-        <figure>
-          <Image/>
-          <Heading3/>
-          <MovieRating/>
-        </figure>
-    </article> );
-}
- 
+  return (
+    <article>
+      <figure>
+        <Image />
+      </figure>
+      <Heading3 />
+      <MovieRating />
+    </article>
+  );
+};
+
 export default MovieCard;
